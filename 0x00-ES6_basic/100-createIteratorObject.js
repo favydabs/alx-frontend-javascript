de19ea0bcad41d66d68a6e8204b16c26e84bde45
favydabs <favourdabit@gmail.com>
2023-11-29 10:48:47 +0000

    ES6 Basics

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,34 +1,9 @@
 export default function createIteratorObject(report) {
-  const iterator = {
-    departments: Object.keys(report),
-    employees: {},
-    currentIndex: 0,
-    next() {
-      const currentDepartment = this.departments[this.currentIndex];
-      const currentEmployees = this.employees[currentDepartment];
-
-      if (!currentEmployees || currentEmployees.length === 0) {
-        // Move to the next department
-        this.currentIndex++;
-
-        if (this.currentIndex < this.departments.length) {
-          return this.next(); // Recursive call to handle empty departments
-        } else {
-          return { done: true };
-        }
+  return (function* _() {
+    for (const department of Object.values(report.allEmployees)) {
+      for (const employee of department) {
+        yield employee;
       }
-
-      const currentEmployee = currentEmployees.shift();
-      return { value: currentEmployee, done: false };
-    },
-    [Symbol.iterator]() {
-      return this;
-    },
-  };
-
-  for (const department in report) {
-    iterator.employees[department] = [...report[department]];
-  }
-
-  return iterator;
+    }
+  }());
 }
