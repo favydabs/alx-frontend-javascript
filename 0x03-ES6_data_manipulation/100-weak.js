7153cea526f30e80d93633b0cdf7c5f2b9f523df
favydabs <favourdabit@gmail.com>
2023-12-05 11:53:45 +0000

    ES6 Manipulation

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,9 +1,10 @@
+export const weakMap = new WeakMap();
+
 export function queryAPI(endpoint) {
-  if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 0);
-  }
-  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
-  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
+  const count = weakMap.get(endpoint) || 0;
+  weakMap.set(endpoint, count + 1);
+
+  if (count >= 4) {
     throw new Error('Endpoint load is high');
   }
 }
